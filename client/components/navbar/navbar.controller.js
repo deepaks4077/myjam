2c89d8def86638026b20bcfde1aea46d6ce96403
deepaks4077 <deepaks4077@gmail.com>
2015-09-20 00:23:16 -0400

    Fixed layout issues, searching and queuing also working. TODO: Fix deletion

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,23 +1,43 @@
 'use strict';
 
 angular.module('myjamApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+.controller('NavbarCtrl', function ($scope, $location, $http, $log, Auth, VideosService) {
+  $scope.menu = [{
+    'title': 'Home',
+    'link': '/'
+  }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+  $scope.isCollapsed = true;
+  $scope.isLoggedIn = Auth.isLoggedIn;
+  $scope.isAdmin = Auth.isAdmin;
+  $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+  $scope.logout = function() {
+    Auth.logout();
+    $location.path('/login');
+  };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+  $scope.isActive = function(route) {
+    return route === $location.path();
+  };
+
+  $scope.search = function (searchValue) {
+    $http.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        key: 'AIzaSyAdLbwPetb6jwTyHDuh1QeVwAYD3cv_7ak',
+        type: 'video',
+        maxResults: '8',
+        part: 'id,snippet',
+        fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+        q: searchValue
+      }
+    })
+    .success( function (data) {
+      VideosService.listResults(data);
+      $log.info(data);
+    })
+    .error( function () {
+      $log.info('Search error');
+    });
+  }
+});
